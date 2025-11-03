6120e70fe95b1937ad4a2356cd735872f5d04684
guantik-gm <guantik.chiang@gmail.com>
2025-11-04 06:50:34 +0800

    update: 优化消息结构

--- hedge/hedge_monitor.py
+++ hedge/hedge_monitor.py
@@ -175,11 +175,11 @@ class HedgeMonitor:
             self.lighter_open_quantity = self.get_current_order().current_lighter_quantity
             self.lighter_open_side = lighter_side
             
-            strategy_msgs = [f"---📋 策略: {strategy.name}--- \n" + "\t\n".join(strategy.get_msgs()) for strategy in strategies]
+            strategy_msgs = [f"----------📋 策略: {strategy.name}---------- \n" + "\t\n".join(strategy.get_msgs()) for strategy in strategies]
             strategy_msg = "【触发策略列表】\n" + "\n".join(strategy_msgs)
             
             # 构建基础通知模板
-            open_msg = f"🔄 [{self.primary_exchange_name}_{self.ticker}] 智能对冲模式 - [开仓执行通知]\n" \
+            open_msg = f"🔄 [{self.primary_exchange_name}_{self.ticker}] 智能对冲模式 - 【开仓执行通知】\n" \
                      f"━━━━━━━━━━━━━━━━━━━━━━\n" \
                      f"🕐 开仓时间: {time.strftime('%Y-%m-%d %H:%M:%S')}\n" \
                      f"🏭 {self.primary_exchange_name} 开仓方向: {self.primary_open_side.upper()}, 持仓数量: {self.primary_open_quantity}, 成交价: ${self.primary_open_price:.6f}\n" \
@@ -278,10 +278,10 @@ class HedgeMonitor:
             if abs(current_primary_position) > Decimal('0') or abs(current_lighter_position) > Decimal('0'):
                 position_warning = "\n🚨 警告：平仓后持仓非零，请手动检查！"
             
-            strategy_msgs = [f"---📋 策略: {strategy.name}--- \n" + "\t\n".join(strategy.get_msgs()) for strategy in strategies]
+            strategy_msgs = [f"}----------📋 策略: {strategy.name}---------- \n" + "\t\n".join(strategy.get_msgs()) for strategy in strategies]
             strategy_msg = "【触发策略列表】\n" + "\n".join(strategy_msgs)
             
-            close_msg = f"🔄 [{self.primary_exchange_name}_{self.ticker}] 智能对冲模式 - [平仓执行通知]\n" \
+            close_msg = f"🔄 [{self.primary_exchange_name}_{self.ticker}] 智能对冲模式 - 【平仓执行通知】\n" \
                       f"━━━━━━━━━━━━━━━━━━━━━━\n" \
                       f"🕐 平仓时间: {time.strftime('%Y-%m-%d %H:%M:%S')}\n" \
                      f"🏭 {self.primary_exchange_name} 开仓方向: {self.primary_open_side.upper()}, 持仓数量: {self.primary_open_quantity}, 成交价: ${self.primary_open_price:.6f}\n" \
