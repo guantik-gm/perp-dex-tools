42d32afd38fd567fd2a940911cbd0164c7d06e61
guantik-gm <guantik.chiang@gmail.com>
2025-11-05 22:58:50 +0800

    fix: PARTIALLY_FILLED状态处理

--- hedge/hedge_mode_abc.py
+++ hedge/hedge_mode_abc.py
@@ -560,9 +560,15 @@ class HedgeBotAbc(ABC):
                             # 取消失败时如果订单状态已经是CANCELED或FILLED，则更新状态，理论上不会再出现卡单状态
                             if cancel_result.status in ['CANCELED', 'FILLED']:
                                 if cancel_result.status == 'FILLED' or cancel_result.filled_size > 0:
-                                    self.logger.info(f"订单取消失败，但订单已全部或部分成交: {cancel_result.filled_size}, 重置 {self.primary_exchange_name} 订单状态为 FILLED")
+                                    self.logger.info(f"订单取消失败，但订单已全部或部分成交: {cancel_result.filled_size}, 重置 {self.primary_exchange_name()} 订单状态为 FILLED")
                                     self.primary_order_status = 'FILLED'
                                     order_data = {'side': cancel_result.side, 'price': cancel_result.price, 'filled_size': cancel_result.filled_size}
+                                    # todo: 这里有很多重复代码可以优化, 还要调用log_trade_to_csv，参考order_update_handler
+                                    # todo: side等值目前都是按照edgex来的，还有其他类似的接口返回值，其他交易所执行需要注意
+                                    if cancel_result.side == "buy":
+                                        self.primary_position += cancel_result.filled_size
+                                    else:
+                                        self.primary_position -= cancel_result.filled_size
                                     self.handle_primary_order_update(order_data)
                                     return HedgeOrderResult.SUCCESS
                                 else:
@@ -581,9 +587,13 @@ class HedgeBotAbc(ABC):
                                 return HedgeOrderResult.SUCCESS
                             # 真实场景下，取消成功后也有可能是部分成交了
                             if cancel_result.status == 'FILLED' or cancel_result.filled_size > 0:
-                                    self.logger.info(f"订单取消成功，但订单已全部或部分成交: {cancel_result.filled_size}, 重置 {self.primary_exchange_name} 订单状态为 FILLED")
+                                    self.logger.info(f"订单取消成功，但订单已全部或部分成交: {cancel_result.filled_size}, 重置 {self.primary_exchange_name()} 订单状态为 FILLED")
                                     self.primary_order_status = 'FILLED'
                                     order_data = {'side': cancel_result.side, 'price': cancel_result.price, 'filled_size': cancel_result.filled_size}
+                                    if cancel_result.side == "buy":
+                                        self.primary_position += cancel_result.filled_size
+                                    else:
+                                        self.primary_position -= cancel_result.filled_size                                    
                                     self.handle_primary_order_update(order_data)
                                     return HedgeOrderResult.SUCCESS
                             # should_cancel说明当前primary下单价格将会改变，重新计算策略条件（比如价差策略）
@@ -617,8 +627,12 @@ class HedgeBotAbc(ABC):
                         # 这里的状态有很多中，不需要再设置一次，下轮循环处理自动根据状态处理
                         if order_info.status == "FILLED" or order_info.filled_size > 0:
                             # 只有FILLED状态需要调用更新
-                            self.logger.info(f"订单已全部或部分成交: {order_info.filled_size}, 重置 {self.primary_exchange_name} 订单状态为 FILLED, 订单信息: {order_info}")
+                            self.logger.info(f"订单已全部或部分成交: {order_info.filled_size}, 重置 {self.primary_exchange_name()} 订单状态为 FILLED, 订单信息: {order_info}")
                             self.primary_order_status = 'FILLED'
+                            if cancel_result.side == "buy":
+                                self.primary_position += cancel_result.filled_size
+                            else:
+                                self.primary_position -= cancel_result.filled_size
                             self.handle_primary_order_update(order_data)
                             # return也可以不需要，下轮循环直接走FILLED分支
                             return HedgeOrderResult.SUCCESS
