6f0b344cc92fe57dc384f1ed56e807aed0a9a64d
guantik-gm <guantik.chiang@gmail.com>
2025-11-06 09:59:30 +0800

    update: 平仓使用max spread进行基准判断

--- hedge/strategy/spread_strategy.py
+++ hedge/strategy/spread_strategy.py
@@ -87,7 +87,7 @@ class SpreadStrategy(HedgeStrategy):
             # 获取当前真实执行价差
             # current_spread = await self.get_realistic_executable_spread(hedge_bot, is_closing=True, sample_count=1, use_max=False)
             # 使用采样区间的最小值
-            current_spread = await self.get_realistic_executable_spread(hedge_bot, is_closing=True, sample_count=self.current_spread_sample_count, use_max=False)
+            current_spread = await self.get_realistic_executable_spread(hedge_bot, is_closing=True, sample_count=self.current_spread_sample_count, use_max=True)
             self.current_spread = current_spread
             self.close_spread = current_spread  # 记录预计平仓价差
             
