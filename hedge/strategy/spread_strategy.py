41439b49f245a74e99a86450410dca76920321f9
guantik-gm <guantik.chiang@gmail.com>
2025-11-07 20:13:21 +0800

    update: spread thredshould

--- hedge/strategy/spread_strategy.py
+++ hedge/strategy/spread_strategy.py
@@ -19,7 +19,7 @@ class SpreadStrategy(HedgeStrategy):
     def __init__(self, priority=10):
         super().__init__(open_priority=priority, close_priority=priority)
         self.current_spread_sample_count = 10  # 当前价差采样次数（默认值）
-        self.profit_threshold = 0.1
+        self.profit_threshold = 0.05
         
         # trading_loop 中将会根据概述性决定primary开仓方向 
         self.open_side = None
